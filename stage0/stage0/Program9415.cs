f019a1da196f9269622e54e9a229de0d662d2a81
michal24263@gmail.com <michal24263@gmail.com>
2023-09-23 23:05:13 +0300

    welcome functions

---
+++ stage0/stage0/Program9415.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9415();
+        }
+
+        static partial void Welcome0063();
+        private static void Welcome9415()
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welocme to my first console application", name);
+        }
+    }
+}

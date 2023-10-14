d32fd4d2a50756502692e94699ed832c24e171b6
Avital Temstet <avitaltem14@gmail.com>
2023-10-15 02:22:09 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program0063.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0063();
+            Welcome9415();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome9415();
+        private static void Welcome0063()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my firt console application", userName);
+        }
+    }
+}

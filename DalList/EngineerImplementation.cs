66788c738bad6f690e3432f33c13e832248358c7
214859415 <214859415@LAB41PC19>
2023-11-06 15:00:11 +0200

    CRUD - start

--- DalList/EngineerImplementation.cs
+++ DalList/EngineerImplementation.cs
@@ -7,26 +7,52 @@ public class EngineerImplementation : IEngineer
 {
     public int Create(Engineer item)
     {
-        throw new NotImplementedException();
+        if (Read(item.Id) != null)
+        {
+            throw new Exception($"Engineer with ID = {item.Id} already exist");
+        }
+        return item.Id;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        if (Read(id) == null)
+        {
+            throw new Exception($"Engineer with ID = {id} does not exist");
+        }
+
     }
 
     public Engineer? Read(int id)
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < DataSourse.Engineers.Count; i++)
+            if (DataSourse.Engineers[i].Id == id)
+                return DataSourse.Engineers[i];
+        return null;
     }
 
     public List<Engineer> ReadAll()
     {
-        throw new NotImplementedException();
+        return new List<Engineer>(DataSourse.Engineers);
     }
 
     public void Update(Engineer item)
     {
-        throw new NotImplementedException();
+        Engineer? engineer = Read(item.Id);
+        if (engineer == null)
+        {
+            throw new Exception($"Engineer with ID = {item.Id} does not exist");
+        }
+        Delete(item.Id);
+        engineer = item;
+
+
+        for (int i = 0; i < DataSourse.Engineers.Count; i++)
+            if (DataSourse.Engineers[i].Id == item.Id)
+            {
+                if(DataSourse.Engineers[i]==null)
+                    throw new Exception($"Engineer with ID = {item.Id} does not exist");
+                DataSourse.Engineers[i]= engineer;
+            }
     }
 }

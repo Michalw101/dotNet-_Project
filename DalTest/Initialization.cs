17f47a95ca69a0dd943f06d3979273543ccca112
Avital Temstet <avitaltem14@gmail.com>
2023-11-09 00:15:34 +0200

    full ERROR

---
+++ DalTest/Initialization.cs
@@ -0,0 +1,42 @@
+﻿namespace DalTest;
+using DalApi;
+using DO;
+
+public static class Initialization
+{
+    private static IEngineer? s_dalEngineer;
+    private static ITask? s_dalTask;
+    private static IDependency? s_dalDependency;
+
+    private static void createEngineers()
+    {
+        RandomGenerator r = new RandomGenerator();
+        string[] names = new string[10];
+        for(int i = 0; i < names.Length; i++)
+        {
+            names[i] = r.GenerateName();
+        }
+
+        foreach (var _name in names)
+        {
+            int _id;
+            do
+                _id = s_rand.Next(MIN_ID, MAX_ID);
+            while (s_dalEngineer!.Read(_id) != null);
+
+            bool? _b = (_id % 2) == 0 ? true : false;
+            Year _year =
+            (Year)s_rand.Next((int)Year.FirstYear, (int)Year.ExtraYear + 1);
+
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            DateTime _bdt = start.AddDays(s_rand.Next(range));
+
+            Student newStu = new(_id, _name, null, _b, _year, _bdt);
+
+            s_dalStudent!.Create(newStu);
+
+        }
+
+    }
+}
---
+++ DalTest/RandomGenerator.cs
@@ -0,0 +1,30 @@
+﻿namespace DalTest;
+internal class RandomGenerator
+{
+    private static readonly Random s_rand = new();
+    public RandomGenerator()
+    {
+        rFirst = new Random(42);
+        rLast = new Random(rFirst.Next());
+    }
+
+    public string GenerateName()
+    {
+        string[] results = new string[2];
+        results[0] = _firstName[rFirst.Next(_firstName.Length)];
+        results[1] = _lastName[rLast.Next(_lastName.Length)];
+        return results[0]+results[1];
+    }
+    Random rFirst;
+    Random rLast;
+
+    string[] _firstName = new string[] { "Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter" };
+
+    string[] _lastName = new string[] { "Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler" };
+
+    public int GenerateID()
+    {
+        int id = s_rand.Next(MIN_ID, MAX_ID);
+        return id;
+    }
+}

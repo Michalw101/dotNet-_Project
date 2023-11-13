91044762eeaf3f188f80fea9cbcadce18a03fbb8
214859415 <214859415@LAB41PC19>
2023-11-13 15:27:08 +0200

    Almost there :)

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -13,9 +13,9 @@ namespace DalTest
         private static void EngineerMethods()
         {
             Console.WriteLine("Enter your choice: \n 0- Exit \n 1- Create \n 2- Read \n 3- ReadAll \n 4- Update \n 5- Delete");
-            int id, cost ;
+            int id, cost;
             int choice = Convert.ToInt32(Console.ReadLine());
-            string? name, email;
+            string? name, email, input;
             Engineer? engineer;
             EngineerExperience level;
             switch (choice)
@@ -25,10 +25,10 @@ namespace DalTest
                     break;
                 case 1:
                     Console.WriteLine("Enter engineer details : id, name, email, level(0 - rookie, 1 - junior, 2 - expert), cost");
-                    id = Convert.ToInt32(Console.ReadLine());
+                    id = Convert.ToInt32(Console.ReadLine() ?? throw new Exception("id can not be null"));
                     name = Console.ReadLine() ?? throw new Exception("name can not be null");
-                    email = Console.ReadLine() ?? throw new Exception("name can not be null");
-                    level = (EngineerExperience)Convert.ToInt32(Console.ReadLine());
+                    email = Console.ReadLine() ?? throw new Exception("email can not be null");
+                    level = (EngineerExperience)Convert.ToInt32(Console.ReadLine() ?? throw new Exception("level can not be null"));
                     cost = Convert.ToInt32(Console.ReadLine());
                     engineer = new Engineer(id, name, email, level, cost);
                     s_dalEngineer!.Create(engineer);
@@ -45,18 +45,18 @@ namespace DalTest
                         Console.WriteLine(item);
                     }
                     break;
-                    case 4:
+                case 4:
                     Console.WriteLine("Enter ID");
                     id = Convert.ToInt32(Console.ReadLine());
                     engineer = s_dalEngineer!.Read(id);
                     Console.WriteLine(engineer);
-                    Console.WriteLine("Enter engineer details : id, name, email, level(0 - rookie, 1 - junior, 2 - expert), cost");
-                    int? input = Console.ReadLine() ?? ;
-                    id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter engineer details : id, name, email, level (0 - rookie, 1 - junior, 2 - expert), cost");
+                    input = Console.ReadLine();
+                    id = (input == "''") ? engineer!.Id : Convert.ToInt32(input);
                     name = Console.ReadLine() ?? engineer!.Name;
                     email = Console.ReadLine() ?? engineer!.Email;
-                    level = (EngineerExperience)Convert.ToInt32(Console.ReadLine());
-                    cost = Convert.ToInt32(Console.ReadLine());
+                    level = (EngineerExperience)Convert.ToInt32(Console.ReadLine() ?? Convert.ToInt32(engineer!.Level).ToString());
+                    cost = Convert.ToInt32(Console.ReadLine() ?? engineer!.Cost.ToString());
                     break;
             }
         }

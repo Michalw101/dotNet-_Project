67d8ad08de78615bee7c222c44f4c7e096a81c6e
Michal <michal24263@gmail.com>
2023-12-25 13:36:43 +0200

    מחשב לא עובד!!!!

--- BL/BO/Engineer.cs
+++ BL/BO/Engineer.cs
@@ -5,14 +5,9 @@ namespace BO;
 public class Engineer
 {
     public int Id { get; init; }
-    public string? Name { get; set; }
+    public string Name { get; set; }
     public string? Email { get; set; }
     public EngineerExperience Level { get; set; }
     public double Cost { get; set; }
-    public Task? task { get; set; }
-
-
-    
-
-
+    public Task? task { get; set; } = null;
 }

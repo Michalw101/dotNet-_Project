be06051c2b5967b419a89bda902c148e9ed4913e
Avital Temstet <avitaltem14@gmail.com>
2024-01-04 11:37:37 +0200

    implemetention - התחלה

--- BL/BIImplementation/EngineerImplementation.cs
+++ BL/BIImplementation/EngineerImplementation.cs
@@ -1,22 +1,67 @@
 ﻿namespace BlImplementation;
 using BlApi;
+using System.Net.Mail;
 
 internal class EngineerImplementation : IEngineer
 {
     private DalApi.IDal _dal = DalApi.Factory.Get;
     public int Create(BO.Engineer item)
     {
-        throw new NotImplementedException();
+        if(item.Id <= 0)
+            throw new BO.BlUnvalidException($"ID = {item.Id} is not valid");
+        if (item.Name == null)
+            throw new BO.BlUnvalidException($"Name = {item.Name} is not valid");
+        if (item.Cost <= 0)
+            throw new BO.BlUnvalidException($"Cost = {item.Cost} is not valid");
+        if(new MailAddress(item.Email) == null)
+            throw new BO.BlUnvalidException($"Email = {item.Cost} is not valid");
+        DO.Engineer doEngineer = new DO.Engineer
+        (item.Id, item.Name, item.Email, (DO.EngineerExperience)item.Level);
+        try
+        {
+            int idEngineer = _dal.Engineer.Create(doEngineer);
+            return idEngineer;
+        }
+        catch (DO.DalAlreadyExistsException ex)
+        {
+            throw new BO.BlAlreadyExistsException($"Engineer with ID={item.Id} already exists", ex);
+        }
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        DO.Engineer? doEngineer = _dal.Engineer.Read(id);
+        if (doEngineer == null)
+            throw new BO.BlDoesNotExistException($"Engineer with ID={id} does Not exist");
+        if (
+        (from DO.Task doTask in _dal.Task.ReadAll()
+         select doTask).FirstOrDefault(element => element.EngineerId == id) != null)
+            throw new BO.BlDeletionImpossibleException($"Engineer with ID = {id} have tasks and canot be deleted");
+        try
+        {
+            _dal.Task.Delete(id);
+        }    
+        catch(DO.DalAlreadyExistsException ex)
+        {
+            throw new BO.BlDeletionImpossibleException($"Engineer with ID = {id} have canot be deleted");
+        }
+
     }
 
     public BO.Engineer? Read(int id)
     {
-        throw new NotImplementedException();
+        DO.Engineer? doEngineer = _dal.Engineer.Read(id);
+        if (doEngineer == null)
+            throw new BO.BlDoesNotExistException($"Engineer with ID={id} does Not exist");
+        return new BO.Engineer()
+        {
+            Id = id,
+            Name = doEngineer.Name,
+            Email = doEngineer.Email,
+            Level = (BO.EngineerExperience)Convert.ToInt32(doEngineer.Level),
+            Cost = doEngineer.Cost
+        };
+
     }
 
     public IEnumerable<BO.Engineer> ReadAll(Func<BO.Engineer, bool>? filter = null)
--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -10,8 +10,28 @@ public class BlDoesNotExistException : Exception
 }
 
 [Serializable]
+public class BlAlreadyExistsException : Exception
+{
+    public BlAlreadyExistsException(string? message) : base(message) { }
+    public BlAlreadyExistsException(string message, Exception innerException)
+                : base(message, innerException) { }
+}
+
+[Serializable]
 public class BlNullPropertyException : Exception
 {
     public BlNullPropertyException(string? message) : base(message) { }
+}
+
+[Serializable]
+public class BlUnvalidException : Exception
+{
+    public BlUnvalidException(string? message) : base(message) { }
+}
+
+[Serializable]
+public class BlDeletionImpossibleException : Exception
+{
+    public BlDeletionImpossibleException(string? message) : base(message) { }
 }
 
